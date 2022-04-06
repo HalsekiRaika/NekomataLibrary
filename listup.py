c41cb7340dcbafa78db125778435f8e3175414a0
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2022-04-06 23:26:33 +0900

    Changed to give the number of people belonging to the group.

--- listup.py
+++ listup.py
@@ -9,11 +9,14 @@ def main():
             if config_dir != ".git":
                 check_dir.append(config_dir)
 
+    af_dir = []
     count = 0
     for configs in check_dir:
+        af_count = 0
         for config_entry in os.scandir(f'./{configs}'):
             if config_entry.name.endswith('.json'):
                 count += 1
+                af_count += 1
                 with open(f'./{configs}/{config_entry.name}', encoding='utf-8_sig') as open_entry:
                     read_entry = json.load(open_entry)
                     id = read_entry['id']
@@ -31,7 +34,8 @@ def main():
                         for channel in channels[1:]:
                             id = channel['id']
                             print(f'{id.rjust(48)}')
-                            
+        af_dir.append((configs, af_count))
+        print(af_dir)
 
 if __name__ == '__main__':
     main()
